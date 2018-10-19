5bc9059a177b81228d824a123503fe991773d37e
tomaszReda <tomasz.reda@xentivo.pl>
2018-10-19 09:12:23 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {DoneTaskComponent} from './done-task/done-task.component';
-import {TodoTaskComponent} from './todo-task/todo-task.component';
-import {AddTaskComponent} from './add-task/add-task.component';
-import {SecretComponent} from './secret/secret.component';
-import {Level1Component} from './secret/level1/level1.component';
-import {Level2Component} from './secret/level2/level2.component';
 import {AppRoutingModule} from './app-routing.module';
-import {AboutComponent} from './about/about.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {HttpClientModule} from '@angular/common/http';
 
 
-
-
-const  config = {
-  apiKey: "AIzaSyAuOnhe8eLq9HPexK2a8feAMWu5qgI0Q2k",
-  authDomain: "angular6test-9b836.firebaseapp.com",
-  databaseURL: "https://angular6test-9b836.firebaseio.com",
-  projectId: "angular6test-9b836",
-  storageBucket: "angular6test-9b836.appspot.com",
-  messagingSenderId: "927695141568"
+const config = {
+  apiKey: 'AIzaSyAuOnhe8eLq9HPexK2a8feAMWu5qgI0Q2k',
+  authDomain: 'angular6test-9b836.firebaseapp.com',
+  databaseURL: 'https://angular6test-9b836.firebaseio.com',
+  projectId: 'angular6test-9b836',
+  storageBucket: 'angular6test-9b836.appspot.com',
+  messagingSenderId: '927695141568'
 };
 
 @NgModule({
