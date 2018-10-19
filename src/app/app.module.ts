5f56d2339db7c2464473d90464c1b5564d6bb8d2
tomaszReda <tomasz.reda@xentivo.pl>
2018-10-19 09:11:46 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,15 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {HttpClientModule} from '@angular/common/http';
 
 
-const config = {
-  apiKey: 'AIzaSyAvKyGAxo-6al5t5lOLJWOck-KrO1d8qSQ',
-  authDomain: 'testowyproject-678a0.firebaseapp.com',
-  databaseURL: 'https://testowyproject-678a0.firebaseio.com',
-  projectId: 'testowyproject-678a0',
-  storageBucket: 'testowyproject-678a0.appspot.com',
-  messagingSenderId: '233391564966'
+
+
+const  config = {
+  apiKey: "AIzaSyAuOnhe8eLq9HPexK2a8feAMWu5qgI0Q2k",
+  authDomain: "angular6test-9b836.firebaseapp.com",
+  databaseURL: "https://angular6test-9b836.firebaseio.com",
+  projectId: "angular6test-9b836",
+  storageBucket: "angular6test-9b836.appspot.com",
+  messagingSenderId: "927695141568"
 };
 
 @NgModule({
